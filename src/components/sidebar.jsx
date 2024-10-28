160713fdb4df7b72793846c88b030c4d2e1384fb
razvanmc15 <razvanmc15@gmail.com>
2024-10-28 16:13:34 +0200

    ceva

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
   return (
-    <div className="align-middle justify-center rounded-lg bg-grey-700 shadow text-red-500 float-right w-70">
+    <div className=" border-2 border-red-900 align- justify-center rounded-lg bg-grey-700 shadow text-red-500 float-right w-70">
       RightSidebar Text
     </div>
   );
