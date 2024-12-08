5cbc8069a9780c880d357c2a95b9301c4500f5bd
moroubb <vlad.morosanu@stud.ubbcluj.ro>
2024-12-08 19:23:15 +0200

    login + signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Sidebar from "./components/Sidebar";
 import MainView from "./components/MainView";
+import Login from "./pages/LoginPage"; 
+import SignUp from "./pages/SignUp"; 
+
 
 function App() {
   return (
     <div className="flex flex-row h-screen ">
-      <Sidebar />
+      {/* { <Sidebar />
       <MainView />
+      <Login />} */}
+      <SignUp />
     </div>
   );
 }
