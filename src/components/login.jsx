1c35deb6ec61c446a9f6329fe7eaec1b7b3f669b
RazvanMacovei15 <razvanmc15@gmail.com>
2024-12-11 01:15:37 +0200

    mesaj de final de zi. Fuck this shit I'm out!

--- src/components/login.jsx
+++
@@ -1,7 +0,0 @@
-export default function Login() {
-  return (
-    <div className="align-middle justify-center rounded-lg bg-grey-700 shadow text-blue-500 float-right w-70">
-      RightSidebar Text
-    </div>
-  );
-}
