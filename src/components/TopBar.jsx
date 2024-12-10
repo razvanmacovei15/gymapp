5a821443fdbcefcbbf7600a89b12d93976960c50
RazvanMacovei15 <razvanmc15@gmail.com>
2024-12-11 01:11:27 +0200

    mesaj de final de zi. Fuck this shit I'm out!

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,14 +1,32 @@
 import HeaderMenu from "./HeaderMenu";
 import UserProfile from "./UserProfile";
 import NotificationAndGymSelector from "./NotificationAndGymSelector";
+import { useAuth } from "./AuthProvider";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function TopBar({ toggleSidebar }) {
+
+  const { authState } = useAuth();
   const locations = ["Gym A", "Gym B", "Gym C", "Gym D"];
-  const username = "John";
+  const username = authState.currentUser.name;
+
+  const [gyms, setGyms] = useState([]);
+
+  const getGyms = async() =>{
+    const response = await axios.get("http://maco-coding.go.ro:8010/gyms/all");
+    const gymNames = response.data.map(gym => gym.name);
+    setGyms(gymNames);
+    return setGyms(response.data);
+  } 
 
   const handleLocationChange = (newLocation) => {
     console.log("Selected location:", newLocation);
   };
+
+  useEffect(() => {
+    getGyms();
+  }, []);
 
   return (
     <header className="h-32 bg-[#455271] rounded-xl mx-1 mt-1 flex justify-between items-center px-8 py-4">
---
+++ src/components/UnauthorizedPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const UnauthorizedPage = () => {
+  const handleGoBack = () => {
+    window.location.href = '/login'; // Redirect to login page
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold text-red-600 mb-4">
+          You are not authorized to be here
+        </h1>
+        <button
+          onClick={handleGoBack}
+          className="px-6 py-3 bg-blue-600 text-white font-medium rounded hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300"
+        >
+          Go back to login page
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UnauthorizedPage;
