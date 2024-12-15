aa29bdb67df7b6104bdda0e70da7d9f85a7ebe33
mihutandra <andra.mihut@stud.ubbcluj.ro>
2024-12-15 12:08:18 +0200

    am schimbat culorile

--- src/components/NotificationAndGymSelector.jsx
+++ src/components/NotificationAndGymSelector.jsx
@@ -13,18 +13,18 @@ export default function NotificationAndGymSelector({ locations, onLocationChange
 
   return (
     <div className="flex items-center gap-4">
-      <FaBell size={24} className="cursor-pointer" />
+      <FaBell size={24} className="cursor-pointer text-white" />
 
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <MenuButton className="inline-flex justify-center gap-x-1.5 rounded-md bg-[#2a333f] px-3 py-2 text-sm font-semibold text-[#c8d4f0] shadow-sm ring-1 ring-inset ring-gray-600 hover:bg-[#465669]">
+          <MenuButton className="inline-flex justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-950 shadow-sm ring-1 ring-inset ring-gray-600 hover:bg-gray-300">
             {selectedLocation}
-            <MdKeyboardArrowDown aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-300" />
+            <MdKeyboardArrowDown aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-950" />
           </MenuButton>
         </div>
 
         <MenuItems
-          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-[#2a333f] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
         >
           <div className="py-1">
             {locations.map((location) => (
@@ -33,7 +33,7 @@ export default function NotificationAndGymSelector({ locations, onLocationChange
                   <button
                     onClick={() => handleLocationChange(location)}
                     className={`${
-                      active ? 'bg-gray-100 text-gray-900' : 'text-[#c8d4f0]'
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-950'
                     } block w-full px-4 py-2 text-left text-sm`}
                   >
                     {location}
