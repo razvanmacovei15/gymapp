b0090498db128e765429820bec4b2bb3cc7fd19e
mihutandra <andra.mihut@stud.ubbcluj.ro>
2024-12-17 19:20:37 +0200

    sidebar

---
+++ src/components/side-bar/app-sidebar.tsx
@@ -0,0 +1,72 @@
+import { Calendar,BookOpenCheck, LayoutDashboard,Dumbbell,ConciergeBell ,CalendarPlus} from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Tasks",
+    url: "#",
+    icon: BookOpenCheck,
+  },
+  {
+    title: "Schedule",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Equipment Maintenance",
+    url: "#",
+    icon: Dumbbell,
+  },
+  {
+    title: "Front Desk",
+    url: "#",
+    icon: ConciergeBell,
+  },
+  {
+    title: "Future Events",
+    url: "#",
+    icon: CalendarPlus,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>My Gym</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
