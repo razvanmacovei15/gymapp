97eda3d9c8816b4c19736a099badf18c09d31fc7
mihutandra <andra.mihut@stud.ubbcluj.ro>
2024-12-19 11:07:39 +0200

    s-a incercat un routing

---
+++ src/pages/UnderDevelopment.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UnderDevelopment() {
+  return (
+    <div className="flex items-center justify-center h-full bg-gradient-to-b from-gray-950 via-gray-950 to-pink-950">
+      <h1 className="text-3xl text-gray-700">This page is under development.</h1>
+    </div>
+  );
+}
