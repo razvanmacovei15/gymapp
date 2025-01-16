a126c7ab051be77c0783fc43b6e78a4058c4da63
razvanmc15 <razvanmc15@gmail.com>
2025-01-16 21:58:40 +0200

    mesaj

--- src/components/task-view/CreatedBy.tsx
+++ src/components/task-view/CreatedBy.tsx
@@ -1,18 +1,12 @@
 import { useAuth } from "../AuthProvider";
-import MiniUserCard from "./MiniUserCard"
+import MiniUserCard from "./MiniUserCard";
 
 export const CreatedBy = () => {
-
-    const { authState } = useAuth();
-    return (
-        <div className="flex flex-row items-center">
-            <p className="w-1/5 text-gray-500">Created by</p>
-            {authState.currentUser && (
-              <MiniUserCard user={authState.currentUser} />
-            )}
-            <p className="text-gray-500 text-sm pl-5">
-              //todo: this needs to change into the real creator of the task
-            </p>
-          </div>
-    )
-}
+  const { authState } = useAuth();
+  return (
+    <div className="flex flex-row items-center">
+      <p className="w-1/5 text-gray-500">Created by</p>
+      {authState.currentUser && <MiniUserCard user={authState.currentUser} />}
+    </div>
+  );
+};
