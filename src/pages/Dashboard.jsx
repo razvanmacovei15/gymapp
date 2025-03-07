f988b1c4b8e2703d2e1bef6dfccab3c0e08e6d8d
RazvanMacovei15 <razvanmc15@gmail.com>
2025-03-07 10:56:32 +0200

    small changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -44,6 +44,7 @@ export default function Dashboard() {
       {error ? (
         <p className="text-red-500 text-center">{error}</p>
       ) : (
+        
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 w-full max-w-[1200px]">
           {gyms.map((gym) => (
             <GymBox
