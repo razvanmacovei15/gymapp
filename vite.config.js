6be354e9ec964ed94182cde26b9475ceae4a23a0
RazvanMacovei15 <razvanmc15@gmail.com>
2025-03-07 13:04:21 +0200

    small changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/gymapp",
   plugins: [react()],
   preview: {
     port: 8020,
