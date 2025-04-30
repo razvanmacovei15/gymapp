adaae30645698d6540ccb3cca463dfafb0d4654f
razvanmacovei15 <razvanmc15@gmai.com>
2025-04-30 10:36:09 +0300

    implementat logo

--- src/components/side-bar/app-sidebar.tsx
+++ src/components/side-bar/app-sidebar.tsx
@@ -19,6 +19,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useAuth } from "../AuthProvider";
@@ -38,44 +39,56 @@ const items = [
 
 export function AppSidebar() {
   const { open } = useSidebar();
-  const apiUrl = import.meta.env.VITE_API_URL;
-
   const { logoUrl, fetchLogo } = useAuth();
+
   useEffect(() => {
     fetchLogo();
   }, []);
 
   return (
-    <Sidebar collapsible="icon">
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarHeader>
-            {open ? (
-              <img
-                src={`${logoUrl}`}
-                alt="My Gym"
-                style={{ width: "100px", height: "100px", borderRadius: "50%" }}
-              />
-            ) : (
-              ""
-            )}
-          </SidebarHeader>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link to={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
+    <div className="flex flex-col h-full">
+      <Sidebar collapsible="icon" className="flex-grow">
+        <SidebarContent className="flex flex-col h-full justify-between">
+          <div>
+            <SidebarGroup>
+              <SidebarHeader>
+                {open ? (
+                  <img
+                    src={`${logoUrl}`}
+                    alt="My Gym"
+                    style={{
+                      width: "100px",
+                      height: "100px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                ) : null}
+              </SidebarHeader>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <Link to={item.url}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </div>
+
+          {/* Bottom button */}
+          <div className="p-4">
+            <Button className="w-full" variant="default">
+              Add Entries
+            </Button>
+          </div>
+        </SidebarContent>
+      </Sidebar>
+    </div>
   );
 }
